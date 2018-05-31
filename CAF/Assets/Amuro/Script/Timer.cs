84f90f2b07ad4a02a08a1bf5a3d211f479367f1b
c17001 amuro <c17001@std.it-college.ac.jp>
2018-05-31 11:26:11 +0900

    タイマーの追加

---
+++ CAF/Assets/Amuro/Script/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+	public float time = 60;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		CountTime();
+	}
+
+	public void CountTime()
+	{
+		time -= Time.deltaTime;
+		GetComponent<Text>().text = time.ToString("F0");
+
+		if (time <= 0)
+		{
+			enabled = false;
+
+			Invoke("Change", 2);
+		}
+	}
+
+	public void Change()
+	{
+		SceneManager.LoadScene("xxxx");
+	}
+
+
+
+}
