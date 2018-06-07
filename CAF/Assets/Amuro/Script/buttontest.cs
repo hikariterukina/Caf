e0fa2d46e5f25867ad62abb5ef50fbc5b293aae1
c17001 amuro <c17001@std.it-college.ac.jp>
2018-06-07 10:53:07 +0900

    standard Assetsを消した ボタンを少し直した(反応するようにした)

---
+++ CAF/Assets/Amuro/Script/buttontest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttontest : MonoBehaviour {
+
+    private int counter = 0;
+
+    void Start()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Button Click: " + counter);
+        counter++;
+    }
+}
--- CAF/Assets/Miyaguni/Scripts/Player.cs
+++ CAF/Assets/Miyaguni/Scripts/Player.cs
@@ -11,7 +11,7 @@ public class Player:NetworkBehaviour {
     public GameObject fleaPlayer;
 
     [ClientCallback]
-    void Update() {
+    public void Update() {
         if (!isLocalPlayer) {
             return;
         }
