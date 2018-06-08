27280adb2e455a1dd857e5e4208e3aa644dd8254
MYCOMPUTER\c17014 <c17014@std.it-college.ac.jp>
2018-06-08 15:13:24 +0900

    Host_Button_test、Client_Button_test、を作成

---
+++ CAF/Assets/TerukinaHikari/Scripts/ServerManagerButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerManagerButton : NetworkBehaviour {
+    public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnRoomHostButton()
+    {
+        canvas.gameObject.SetActive(false);
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void OnClientButton()
+    {
+        canvas.gameObject.SetActive(false);
+        NetworkManager.singleton.StartClient();
+    }
+}
---
+++ CAF/Assets/TerukinaHikari/Scripts/yuya.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yuya : MonoBehaviour {
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vec3 = target.eulerAngles;
+		
+	}
+
+   
+}
